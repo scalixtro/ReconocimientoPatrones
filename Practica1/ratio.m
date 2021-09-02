14d76266363499ab5483422b86657ebe9e501701
AlanLopez1017 <87053832+AlanLopez1017@users.noreply.github.com>
2021-09-02 01:44:33 -0500

    Add files via upload

---
+++ Practica1/ratio.m
@@ -0,0 +1,32 @@
+function [cociente] = ratio(img)
+imshow(img)
+
+% Componentes
+img_r = img(:,:,1);
+img_g = img(:,:,2);
+img_b = img(:,:,3);
+
+% Imagenes
+% figure(1)
+% subplot(2,2,1)
+% imshow(img_r)
+% subplot(2,2,2)
+% imshow(img_g)
+% subplot(2,2,3)
+% imshow(img_b)
+
+% Histogramas
+% figure(2)
+% subplot(2,2,1)
+% imhist(img_r)
+% subplot(2,2,2)
+% imhist(img_g)
+% subplot(2,2,3)
+% imhist(img_b)
+
+% Máximos del histograma
+[maxim_r,pos_r] = max(imhist(img_r));
+[maxim_g,pos_g] = max(imhist(img_g));
+cociente = pos_g/pos_r;
+end
+
