18996ee98f9e0851adc95546b1853cdba78ceeed
Said Calixtro <saidcalixtro@gmail.com>
2021-09-07 14:10:44 -0500

    Agregados los archivos

---
+++ Practica1/Reconocedor_signos.m
@@ -0,0 +1,45 @@
+clear all, close all, clc
+
+% Lectura y binarizacion de la imagen
+img = imread('./binarios/Manzana6.jpg');
+img_bin = binarize(img, 'white');
+
+% Obtencion de caracteristicas
+img_color = color(img);
+img_forma = forma(img_bin);
+v = [img_color img_forma]';
+
+% Grafica los datos de las características de color y forma de la manzana y naranja
+
+% Datos de las manzanas rojas
+x1 = [0.5417 0.4500 0.5139 0.3625 0.3867]; % Color
+y1 = [0.3883 0.4637 0.4046 0.4376 0.4270]; % Forma
+
+% Datos de las naranjas 
+x2 = [0.8696 0.8333 0.8672 0.8904 0.8906]; % Color
+y2 = [0.5065 0.5237 0.4601 0.4235 0.5135]; % Forma
+
+
+% Función discriminante
+fd = img_forma + 1.013*img_color-1.136;
+
+if fd > 0 
+    disp("Es una naranja")
+else 
+    disp("Es una manzana")
+end
+
+% Recta
+x = 0.3:0.01:1;
+y = -1.013*x+1.136;
+
+% Grafica
+figure
+plot(x1,y1,'r*',x2,y2,'bo',x,y,'g-',img_color,img_forma,'kx','LineWidth',2)
+
+% Elementos de la gráfica 
+title('Clasificación de frutas')
+xlabel('Color')
+ylabel('Forma')
+legend('Manzanas rojas','Naranjas', 'Recta fd')
+axis([0.3 1 0.38 0.54])
---
+++ Practica1/forma.m
@@ -0,0 +1,31 @@
+function [r_forma] = forma(img_bin)
+    [x,y] = size(img_bin);
+    maximo = 0;
+    altura_fruta = 0;
+    altura_ancho = 0;
+    [i,j] = find(img_bin==1);
+    dif = min(i)-1;
+    for i = 1:x
+        contador = 0;
+        for j = 1:y
+            if img_bin(i,j) == 1
+                contador = contador+1;
+            end
+        end
+
+        max_fila = contador;
+        if max_fila>=maximo
+            maximo = max_fila;
+            altura_ancho = i;
+        end
+        if contador ~= 0
+            altura_fruta = altura_fruta+1;
+        end
+
+    end
+
+    altura_ancho = altura_ancho-dif;
+    r_forma = altura_fruta/altura_ancho;
+
+end
+
