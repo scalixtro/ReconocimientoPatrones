65e4390a72ff0fe990a6df87c0da79c8a616cfec
Said Calixtro <saidcalixtro@gmail.com>
2021-09-07 19:23:47 -0500

    Funciones

---
+++ Practica1/show_rgb.m
@@ -0,0 +1,34 @@
+function [] = show_rgb(img)
+    % Funcion que muestra la imagen en sus componentes RGB a color
+    
+    % Creamos los arrays de las imagenes
+    img_size = size(img);
+    img_r = zeros(img_size);
+    img_g = zeros(img_size);
+    img_b = zeros(img_size);
+    img = im2double(img);
+    
+    for i = 1:img_size(1)
+        for j = 1:img_size(2)
+            img_r(i,j,1) = img(i,j,1);
+            img_g(i,j,2) = img(i,j,2);
+            img_b(i,j,3) = img(i,j,3);
+        end
+    end
+    
+    figure;
+    subplot(2,2,1);
+    imshow(img);
+    title('Imagen original');
+    subplot(2,2,2);
+    imshow(img_r);
+    title('Imagen en rojo');
+    subplot(2,2,3);
+    imshow(img_g);
+    title('Imagen en verde');
+    subplot(2,2,4);
+    imshow(img_b);
+    title('Imagen en azul');
+    
+end
+
---
+++ Practica1/show_rgb_gray.m
@@ -0,0 +1,14 @@
+function [] = show_rgb_gray(img)
+    img = im2double(img);
+    figure;
+    subplot(2,2,1);
+    imshow(img(:,:,1));
+    title('Intensidades de rojo');
+    subplot(2,2,2);
+    imshow(img(:,:,2));
+    title('Intensidades de verde');
+    subplot(2,2,3);
+    imshow(img(:,:,3));
+    title('Intensidades de azul');
+end
+
